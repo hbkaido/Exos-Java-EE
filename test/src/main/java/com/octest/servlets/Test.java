81f54f33f59be57dbbf08ee4833f5bdde743feba
luqman <luqman@DESKTOP-H0K0C9N>
2021-05-23 16:48:00 +0200

    Test

--- test/src/main/java/com/octest/servlets/Test.java
+++ test/src/main/java/com/octest/servlets/Test.java
@@ -1,7 +1,6 @@
 package com.octest.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,24 +8,48 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.octest.beans.Auteur;
+import com.octest.beans.Utilisateur;
+import com.octest.dao.*;
 
-
+/**
+ * Servlet implementation class Test
+ */
 @WebServlet("/Test")
 public class Test extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    private UtilisateurDao utilisateurDao;
 
+    public void init() throws ServletException {
+        DaoFactory daoFactory = DaoFactory.getInstance();
+        this.utilisateurDao = daoFactory.getUtilisateurDao();
+    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Auteur auteur = new Auteur();
-        auteur.setPrenom("Mathieu");
-        auteur.setNom("Nebra");
-        auteur.setActif(true);
-        
-        request.setAttribute("auteur", auteur);
-        
+        try {
+            request.setAttribute("utilisateurs", utilisateurDao.lister());
+        }
+        catch (DaoException e) {
+            request.setAttribute("erreur", e.getMessage());
+        }
         this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
     }
 
+    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        try {
+            Utilisateur utilisateur = new Utilisateur();
+            utilisateur.setNom(request.getParameter("nom"));
+            utilisateur.setPrenom(request.getParameter("prenom"));
+            
+            utilisateurDao.ajouter(utilisateur);
+            request.setAttribute("utilisateurs", utilisateurDao.lister());
+        }
+        catch (Exception e) {
+            request.setAttribute("erreur", e.getMessage());
+        }
+        
+        this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
+    }
+    
+    
 
 }
